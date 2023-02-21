0bc76d330359f912799937d35c1fec18d041cf9e
heminsabah <heminsabahomer@gmail.com>
2023-02-21 18:50:26 +0300

    updated

--- sript.js
+++ sript.js
@@ -14,7 +14,10 @@ async function fetchProducts() {
   
   const promise = fetchProducts();
   promise.then((data) => {
-    const card = document.querySelector(".card" );
+    const card = document.querySelector(".cards" );
+    // const list = document.querySelector(".list")
+    // const list = document.querySelector("h3" );
+    // const listEl = document.querySelector("li" );
     
   
     data.results.forEach((item) => {
@@ -31,13 +34,32 @@ async function fetchProducts() {
       // console.log(item);
   
       if(item.status=="Alive"){
-        // console.log(item);
+        console.log(item);
+        
+        // const listelement = document.createElement("h3");
+        
+       
+        // listelement.classList.add("item");
         const element = document.createElement("div");
+        const imgElement = document.createElement("img");
+        imgElement.src=item.image;
+        const listElement = document.createElement("ul");
+      const listItem = document.createElement("li")
+      // listElement.textContent= item;
         element.classList.add("item");
+        // list.classList.add("card");
+        
+        listElement.classList.add("element");
+        listItem.classList.add("list")
+        
+        // ","+"id:"+ item.id+" "+" name: "+item.name+" status: "+item.status+" species:"+item.species+" type:"+item.type;
+      element.innerHTML=`<h5>Caracter:  ${item.caracter} </br> id:${ item.id}</br>  name:  ${ item.name}</br>  status:  ${ item.status}</br> species:  ${ item.species} </br></h5>`
+      card.appendChild(element);
+      element.appendChild(imgElement);    
       
-      
-      element.textContent="Caracter:"+item.caracter+","+"id:"+ item.id+" "+" name: "+item.name+" status: "+item.status+" species:"+item.species+" type:"+item.type;
-        card.appendChild(element);
+      element.appendChild(listElement);
+      // listElement.appendChild(listItem);
+     
   
       }
   
